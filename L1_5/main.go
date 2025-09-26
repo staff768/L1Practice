184dacc7cff5a60548a360bf60ffc98aca2b634b
staff768 <e.kirpichev2006@gmail.com>
2025-09-26 15:37:57 +0300

    add l1_5

--- L1_5/main.go
+++ L1_5/main.go
@@ -1,9 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
-	in := make(chan int)
-	out := make(chan int)
+func main() {
+	start := time.Now()
+	N := 3
+	timeout := time.After(time.Duration(N) * time.Second)
 
-	for i := r
+	dataChan := make(chan int)
+
+	go func() {
+		i := 0
+		for {
+			select {
+			case <-timeout:
+				close(dataChan)
+				return
+			default:
+				dataChan <- i
+				i++
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+
+	for value := range dataChan {
+		elapsed := time.Since(start)
+		fmt.Printf("Прочитано - %d (Время -  %v)\n", value, elapsed.Truncate(time.Millisecond))
+	}
+
+	fmt.Printf("Конец. Общее время работы: %v\n", time.Since(start).Truncate(time.Millisecond))
 }
